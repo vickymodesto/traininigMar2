496506f0e9947ff7f4a6187de715299a3f89723a
Vicky <vickymodestotech@gmail.com>
2023-03-03 02:05:23 +0000

    Updated

--- databaseconnection.java
+++ databaseconnection.java
@@ -1,4 +1,4 @@
 //Database connection to postgres
-//
+// Updated comments
 System.out.println("DB connected successfully");
 System.out.println("Logic to execute SQL");
--- echo.sh
+++ echo.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 echo "Hello World!"
+
+for VAR in 0 1 2 3 4 5
+do
+	echo $VAR
+done
